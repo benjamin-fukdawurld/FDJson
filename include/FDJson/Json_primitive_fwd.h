6537ce3fde88dd3c3a0d883039e5b91d1896903b
Benjamin ZAMOUR <benjamin.zamour@gmail.com>
2020-04-28 03:04:57 +0200

    Added export macros to functions

--- include/FDJson/Json_primitive_fwd.h
+++ include/FDJson/Json_primitive_fwd.h
@@ -11,30 +11,32 @@
 
 #include <FDJson/FDJson.h>
 
+#include <FDCore/Macros.h>
+
 namespace FDJson
 {
     template<typename T>
     bool unserialize(const rapidjson::Value &val, std::optional<T> &opt, Serializer &serializer, std::string *err = nullptr);
 
-    rapidjson::Value serialize(std::nullptr_t, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(std::nullptr_t, Serializer &serializer);
 
-    rapidjson::Value serialize(const char c, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(const char c, Serializer &serializer);
 
-    bool unserialize(const rapidjson::Value &val, char &c, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, char &c, Serializer &serializer, std::string *err = nullptr);
 
-    rapidjson::Value serialize(const char *c, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(const char *c, Serializer &serializer);
 
-    rapidjson::Value serialize(const std::string &c, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(const std::string &c, Serializer &serializer);
 
-    bool unserialize(const rapidjson::Value &val, std::string &str, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, std::string &str, Serializer &serializer, std::string *err = nullptr);
 
-    rapidjson::Value serialize(const bool b, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(const bool b, Serializer &serializer);
 
-    rapidjson::Value serialize(const float f, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(const float f, Serializer &serializer);
 
-    rapidjson::Value serialize(const double &d, Serializer &serializer);
+    FD_EXPORT rapidjson::Value serialize(const double &d, Serializer &serializer);
 
-    bool unserialize(const rapidjson::Value &val, bool &b, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, bool &b, Serializer &serializer, std::string *err = nullptr);
 
     template<typename T>
     std::enable_if_t<std::is_same<T, int16_t>::value
@@ -50,18 +52,18 @@ namespace FDJson
                          || std::is_same<T, int32_t>::value,
     bool> unserialize(const rapidjson::Value &val, T &i, Serializer &serializer, std::string *err = nullptr);
 
-    bool unserialize(const rapidjson::Value &val, int64_t &i, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, int64_t &i, Serializer &serializer, std::string *err = nullptr);
 
     template<typename T>
     std::enable_if_t<std::is_same<T, uint16_t>::value
                          || std::is_same<T, uint32_t>::value,
     bool> unserialize(const rapidjson::Value &val, T &i, Serializer &serializer, std::string *err = nullptr);
 
-    bool unserialize(const rapidjson::Value &val, uint64_t &i, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, uint64_t &i, Serializer &serializer, std::string *err = nullptr);
 
-    bool unserialize(const rapidjson::Value &val, float &f, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, float &f, Serializer &serializer, std::string *err = nullptr);
 
-    bool unserialize(const rapidjson::Value &val, double &d, Serializer &serializer, std::string *err = nullptr);
+    FD_EXPORT bool unserialize(const rapidjson::Value &val, double &d, Serializer &serializer, std::string *err = nullptr);
 }
 
 #endif // JSON_PRIMITIVE_FWD_H
